4a4d7cd0d34c1a5e0e0d601787e10434f17bd246
sanieni6 <luissanchezzamora23@gmail.com>
2023-10-14 14:58:01 -0500

    Create the store

--- src/store/acta.js
+++ src/store/acta.js
@@ -1,32 +1,58 @@
-import axios from 'axios'
+import axios from "axios";
 
 //mediante este endpoint puedo obtener todos los candidatos
 export const createActa = (set) => ({
-    votesPerProvince: null,
+  votesPerProvince: null,
 
-    //mediante este metodo puedo subir un acta
-    postActa: async (data) => {
-        try {
-            const response = await axios.post('http://52.200.0.69:5000/api/v1/votes/', data)
-            if (response.ok) {
-                return true
-            }
-        } catch (error) {
-            console.error(error)
-        }
-    },
-
-    //mediante este metodo puedo obtener los votos por provincia
-    getVotesPerProvince: async (province) => {
-        try {
-            const response = await axios.get(`http://52.200.0.69:5000/api/v1/votes/${province}`)
-            if (response.ok) {
-                set({ votesPerProvince: response.data })
-            }
-        } catch (error) {
-            console.error(error)
-        }
+  //mediante este metodo puedo subir un acta
+  postActa: async (data) => {
+    try {
+    //   const response = await axios.post(
+    //     "http://52.200.0.69:5000/api/v1/votes/",
+    //     data
+    //   );
+    const response = await fetch('http://52.200.0.69:5000/api/v1/votes/', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data),
+            });
+      if (response.ok) {
+        const data = await response.json();
+        console.log(await data);
+        return true;
+      }
+    } catch (error) {
+      console.error(error);
     }
+  },
 
-    
-})
+  //mediante este metodo puedo obtener los votos por provincia
+  getVotesPerProvince: async (province) => {
+    console.log(province);
+    // URL de la solicitud GET
+    let url = `http://52.200.0.69:5000/api/v1/votes/?province=${province}`;
+    if (province === "Ecuador") {
+        url = `http://52.200.0.69:5000/api/v1/votes/`;
+    }
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    try {
+      const response = await axios.get(url, config);
+      console.log(await response);
+      if (response.status === 201) {
+        const votes = await response.data;
+        set({ votesPerProvince: votes });
+        console.log(await votes);
+      } else {
+        throw new Error("Invalid email or password");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  },
+});
--- src/store/user.js
+++ src/store/user.js
@@ -1,9 +1,15 @@
 import { create } from 'zustand';
 import axios from 'axios';
 
-export const useAuthStore = (set) => ({
-    currentUser: null,
-    isLoggedIn: false,
+const $LOCAL_LOGGEDIN_KEY = "my_app_logged_in";
+const $LOCAL_ROLE_KEY = "my_app_role";
+
+export const useAuthStore = (set) => (
+    
+    {
+    currentUser:  null,
+    isLoggedIn: localStorage.getItem($LOCAL_LOGGEDIN_KEY) ||false,
+    role: localStorage.getItem($LOCAL_ROLE_KEY) || null,
     login: async (username, password) => {
         try {
             const response = await fetch('http://52.200.0.69:5000/api/v1/auth/login', {
@@ -14,9 +20,13 @@ export const useAuthStore = (set) => ({
                 body: JSON.stringify({ username, password }),
             });
             if (response.ok) {
+                const user = await response.json();
                 set({ isLoggedIn: true });
-                set({ currentUser: await response.json()})
-                
+                set({ currentUser: user })
+                set({ role: user.role })
+                localStorage.setItem($LOCAL_LOGGEDIN_KEY, true);
+                localStorage.setItem($LOCAL_ROLE_KEY, user.role);
+                console.log( await response);
             } else {
                 throw new Error('Invalid email or password');
             }
@@ -24,5 +34,11 @@ export const useAuthStore = (set) => ({
             console.error(error);
         }
     },
+    logout: () => {
+        set({ isLoggedIn: false });
+        set({ currentUser: null });
+        set({ role: null });
+        localStorage.removeItem($LOCAL_LOGGEDIN_KEY);
+        localStorage.removeItem($LOCAL_ROLE_KEY);
+    },
 });
-
